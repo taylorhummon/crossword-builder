8713a65f85a2e609450e39d87aba5c6068538790
Ben Hummon <ben.hummon@gmail.com>
2021-05-12 12:31:50 -0500

    remove turns and winning logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,10 +8,6 @@ class Game extends React.Component {
         <div className="game-board">
           <Board />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
       </div>
     );
   }
@@ -22,14 +18,12 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true,
     };
   }
     
   render() {
     return (
       <div>
-        <div className="status">{this.status()}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -60,46 +54,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (squares[i] || this.calculateWinner()) return;
-    squares[i] = this.turn();
-    const xIsNext = ! this.state.xIsNext;
-    this.setState({ squares, xIsNext });
-  }
-
-  status() {
-    const winner = this.calculateWinner();
-    if (winner) {
-      return `Winner: ${winner}`;
-    } else {
-      return `Next player: ${this.turn()}`;
-    }
-  }
-
-  calculateWinner() {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    const squares = this.state.squares;
-    for (const line of lines) {
-      const [a, b, c] = line;
-      for (const value of ['X', 'O']) {
-        if (squares[a] === value && squares[b] === value && squares[c] === value) {
-          return value;
-        }
-      }
-    }
-    return null;
-  }
-
-  turn() {
-    return this.state.xIsNext ? 'X' : 'O';
+    if (squares[i]) return;
+    squares[i] = 'X';
+    this.setState({ squares });
   }
 }
 
