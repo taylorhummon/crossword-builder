b9714ec7c3cfa03d37d57b55ed433533626d52c5
Ben Hummon <ben.hummon@gmail.com>
2021-05-17 14:33:01 -0500

    smarter suggestions

--- src/services/suggestions.js
+++ src/services/suggestions.js
@@ -6,21 +6,18 @@ import dictionary5 from './dictionary5';
 import dictionary6 from './dictionary6';
 
 function computeHorizontalSuggestions(squares, activeIndex, boardWidth) {
-  const patternAndIndex = computePatternAndIndex(squares, activeIndex, boardWidth);
-  const suggestions = findSuggestions(patternAndIndex.pattern, patternAndIndex.index);
+  const pattern = computePatternA(squares, activeIndex, boardWidth);
+  const suggestions = findSuggestions(pattern);
   console.log('SUGGESTIONS', suggestions);
   return suggestions;
 }
 
-function computePatternAndIndex(squares, activeIndex, boardWidth) {
+function computePatternA(squares, activeIndex, boardWidth) {
   const activeIndexColumn = activeIndex % boardWidth;
   const activeIndexRow = (activeIndex - activeIndexColumn) / boardWidth;
   const leftIndex = findLeftIndex(squares, boardWidth, activeIndexRow, activeIndexColumn);
   const rightIndex = findRightIndex(squares, boardWidth, activeIndexRow, activeIndexColumn);
-  return {
-    pattern: computePattern(squares, boardWidth, activeIndexRow, activeIndexColumn, leftIndex, rightIndex),
-    index: activeIndexColumn - leftIndex
-  };
+  return computePatternB(squares, boardWidth, activeIndexRow, activeIndexColumn, leftIndex, rightIndex);
 }
 
 function findLeftIndex(squares, boardWidth, activeIndexRow, activeIndexColumn) {
@@ -39,11 +36,11 @@ function findRightIndex(squares, boardWidth, activeIndexRow, activeIndexColumn) 
   return rightIndex;
 }
 
-function computePattern(squares, boardWidth, activeIndexRow, activeIndexColumn, leftIndex, rightIndex) {
+function computePatternB(squares, boardWidth, activeIndexRow, activeIndexColumn, leftIndex, rightIndex) {
   let pattern = '';
   for (let i = leftIndex; i <= rightIndex; i++) {
     if (i === activeIndexColumn) {
-      pattern += '.';
+      pattern += '@';
     } else {
       const char = squareAt(squares, boardWidth, i, activeIndexRow);
       if (char === null) {
@@ -60,18 +57,63 @@ function squareAt(squares, boardWidth, i, j) {
   return squares[j * boardWidth + i];
 }
 
-const findSuggestions = findSuggestionsA;
+function findSuggestions(pattern) {
+  const initialTimeStamp = Date.now();
+  const suggestions = toAlphabetArray(findSuggestionsA(pattern));
+  console.log('Search Took', Date.now() - initialTimeStamp);
+  return suggestions;
+}
 
-function findSuggestionsA(pattern, index) {
-  const regExp = new RegExp(`^${pattern}$`);
+function findSuggestionsA(pattern) {
+  const alphabetSet = new Set();
+  const subpatterns = computeSubpatterns(pattern);
+  for (const subpattern of subpatterns) {
+    const alphabetSetForSubpattern = findSuggestionsB(subpattern);
+    for (const letter of alphabetSetForSubpattern) {
+      alphabetSet.add(letter);
+    }
+  }
+  return alphabetSet;
+}
+
+function computeSubpatterns(pattern) {
+  const index = pattern.indexOf('@');
+  if (index === -1) throw new Error('Did not find @ in pattern');
+  const leftPoints = [];
+  for (let i = 0; i < index; i++) {
+    if (pattern[i] === '.') leftPoints.push(i + 1);
+  }
+  leftPoints.unshift(0);
+  const rightPoints = [];
+  for (let i = index + 1; i < pattern.length; i++) {
+    if (pattern[i] === '.') rightPoints.push(i);
+  }
+  rightPoints.push(pattern.length);
+  const subpatterns = [];
+  for (const leftPoint of leftPoints) {
+    for (const rightPoint of rightPoints) {
+      subpatterns.push(pattern.substring(leftPoint, rightPoint));
+    }
+  }
+  subpatterns.sort( // !!! we're not taking advantage of this sort for optimization yet
+    (subpatternA, subpatternB) => subpatternA.length - subpatternB.length
+  );
+  return subpatterns;
+}
+
+function findSuggestionsB(pattern) {
+  const index = pattern.indexOf('@');
+  if (index === -1) throw new Error('Did not find @ in pattern');
+  const newPattern = pattern.split('').map(
+    character => character === '@' ? '.' : character
+  ).join('');
+  const regExp = new RegExp(`^${newPattern}$`);
   const alphabetSet = new Set();
   const dictionary = dictionaryWithWordsOfLength(pattern.length);
-  // const initialTimeStamp = Date.now();
   for (const word of dictionary) {
     if (regExp.test(word)) alphabetSet.add(word.charAt(index));
   }
-  // console.log('Search Took', Date.now() - initialTimeStamp);
-  return toAlphabetArray(alphabetSet);
+  return alphabetSet;
 }
 
 function dictionaryWithWordsOfLength(length) {
@@ -81,7 +123,7 @@ function dictionaryWithWordsOfLength(length) {
   if (length === 4) return dictionary4;
   if (length === 5) return dictionary5;
   if (length === 6) return dictionary6;
-  throw 'pattern too long!';
+  throw new Error('Need a dictionary with longer words');
 }
 
 function toAlphabetArray(set) {
