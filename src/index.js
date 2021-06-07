f1a4d1f0dc61226c30cea7d362face65cb87b7e5
Ben Hummon <ben.hummon@gmail.com>
2021-06-07 11:37:00 -0500

    get spectre icons working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import 'spectre.css';
+import './spectre/spectre.min.css';
+import './spectre/spectre-icons.min.css';
 import './index.css';
 
 ReactDOM.render(
