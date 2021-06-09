4f3fc0a88b9ad9eccb360c38838e2b372a5062cc
Ben Hummon <ben.hummon@gmail.com>
2021-06-09 10:20:59 -0500

    create appKeyPresss service

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,10 @@ import Board from './Board';
 import Options from './Options';
 import Suggestions from './Suggestions';
 import Help from './Help';
+import { boardWidth, boardHeight, isMouseNavigation } from '../services/boardNavigation';
+import { updateStateDueToKeyPress } from '../services/appKeyPress';
 import { computeSuggestionsPromise } from '../services/suggestions';
-import {
-  boardWidth, boardHeight,
-  isArrowKey, indexDeterminedByArrowKey,
-  indexOneBeforeActive, indexOneAfterActive,
-  isMouseNavigation
-} from '../services/boardNavigation';
-import { arrayOfSize, arrayShallowCopy, arrayShallowEquivalent } from '../utilities/arrays';
-import { isLetter, filledSquareCharacter } from '../utilities/alphabet';
+import { arrayOfSize, arrayShallowEquivalent } from '../utilities/arrays';
 import './App.css';
 
 class App extends React.Component {
@@ -164,65 +159,6 @@ class App extends React.Component {
     if (! arrayShallowEquivalent(data.squareValues, state.squareValues)) return true;
     return false;
   }
-}
-
-function updateStateDueToKeyPress(prevState, event) {
-  if (event.altKey || event.ctrlKey || event.metaKey) return;
-  const { key } = event;
-  if (isArrowKey(key))      return updateDueToArrowKey(prevState, key);
-  if (isLetter(key))        return updateDueToLetterKey(prevState, key);
-  if (key === 'Enter')      return updateDueToEnterKey(prevState);
-  if (key === ' ')          return updateDueToSpaceKey(prevState);
-  if (key === 'Backspace')  return updateDueToBackspaceKey(prevState);
-  if (key === 'Delete')     return updateDueToDeleteKey(prevState);
-  return null;
-}
-
-function updateDueToArrowKey(prevState, key) {
-  return {
-    activeSquareIndex: indexDeterminedByArrowKey(prevState, false, key)
-  };
-}
-
-function updateDueToLetterKey(prevState, key) {
-  return {
-    squareValues: updateSquare(prevState.squareValues, prevState.activeSquareIndex, key.toUpperCase()),
-    activeSquareIndex: indexOneAfterActive(prevState, true)
-  };
-}
-
-function updateDueToEnterKey(prevState) {
-  return updateDueToSpaceKey(prevState);
-}
-
-function updateDueToSpaceKey(prevState) {
-  return {
-    squareValues: updateSquare(prevState.squareValues, prevState.activeSquareIndex, filledSquareCharacter),
-    activeSquareIndex: indexOneAfterActive(prevState, true)
-  };
-}
-
-function updateDueToBackspaceKey(prevState) {
-  if (prevState.squareValues[prevState.activeSquareIndex] !== null) {
-    return updateDueToDeleteKey(prevState);
-  }
-  const willMoveFocusTo = indexOneBeforeActive(prevState, true);
-  return {
-    squareValues: updateSquare(prevState.squareValues, willMoveFocusTo, null),
-    activeSquareIndex: willMoveFocusTo
-  };
-}
-
-function updateDueToDeleteKey(prevState) {
-  return {
-    squareValues: updateSquare(prevState.squareValues, prevState.activeSquareIndex, null)
-  };
-}
-
-function updateSquare(prevSquareValues, index, value) {
-  const squareValues = arrayShallowCopy(prevSquareValues);
-  squareValues[index] = value;
-  return squareValues;
 }
 
 export default App;
---
+++ src/services/appKeyPress.js
@@ -0,0 +1,65 @@
+import {
+  isArrowKey, indexDeterminedByArrowKey,
+  indexOneBeforeActive, indexOneAfterActive
+} from '../services/boardNavigation';
+import { isLetter, filledSquareCharacter } from '../utilities/alphabet';
+import { arrayShallowCopy } from '../utilities/arrays';
+
+export function updateStateDueToKeyPress(prevState, event) {
+  if (event.altKey || event.ctrlKey || event.metaKey) return;
+  const { key } = event;
+  if (isArrowKey(key))      return updateDueToArrowKey(prevState, key);
+  if (isLetter(key))        return updateDueToLetterKey(prevState, key);
+  if (key === 'Enter')      return updateDueToEnterKey(prevState);
+  if (key === ' ')          return updateDueToSpaceKey(prevState);
+  if (key === 'Backspace')  return updateDueToBackspaceKey(prevState);
+  if (key === 'Delete')     return updateDueToDeleteKey(prevState);
+  return null;
+}
+
+function updateDueToArrowKey(prevState, key) {
+  return {
+    activeSquareIndex: indexDeterminedByArrowKey(prevState, false, key)
+  };
+}
+
+function updateDueToLetterKey(prevState, key) {
+  return {
+    squareValues: updateSquare(prevState.squareValues, prevState.activeSquareIndex, key.toUpperCase()),
+    activeSquareIndex: indexOneAfterActive(prevState, true)
+  };
+}
+
+function updateDueToEnterKey(prevState) {
+  return updateDueToSpaceKey(prevState);
+}
+
+function updateDueToSpaceKey(prevState) {
+  return {
+    squareValues: updateSquare(prevState.squareValues, prevState.activeSquareIndex, filledSquareCharacter),
+    activeSquareIndex: indexOneAfterActive(prevState, true)
+  };
+}
+
+function updateDueToBackspaceKey(prevState) {
+  if (prevState.squareValues[prevState.activeSquareIndex] !== null) {
+    return updateDueToDeleteKey(prevState);
+  }
+  const willMoveFocusTo = indexOneBeforeActive(prevState, true);
+  return {
+    squareValues: updateSquare(prevState.squareValues, willMoveFocusTo, null),
+    activeSquareIndex: willMoveFocusTo
+  };
+}
+
+function updateDueToDeleteKey(prevState) {
+  return {
+    squareValues: updateSquare(prevState.squareValues, prevState.activeSquareIndex, null)
+  };
+}
+
+function updateSquare(prevSquareValues, index, value) {
+  const squareValues = arrayShallowCopy(prevSquareValues);
+  squareValues[index] = value;
+  return squareValues;
+}
