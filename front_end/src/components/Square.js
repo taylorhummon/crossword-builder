2aa02f80f3be4d28be1e6899672a9d2b2043c649
Taylor Hummon <taylor.hummon@gmail.com>
2024-07-26 05:43:04 -0400

    modernizing <Help>, <Square>, and <Suggestions> components

--- front_end/src/components/Square.js
+++ front_end/src/components/Square.js
@@ -1,41 +1,35 @@
-import React from 'react';
 import { filledSquareCharacter } from '../utilities/alphabet';
 import './Square.css';
 
-class Square extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    if (this.props.value !== nextProps.value) return true;
-    if (this.props.isActive !== nextProps.isActive) return true;
-    return false;
-  }
-
-  render() {
-    return (
-      <div
-        className={this._className(this.props)}
-        onClick={this.props.handleSquareClick}
-        data-testid={this.props.dataTestid}
-      >
-        {this._displayedValue()}
-      </div>
-    );
-  }
-
-  _displayedValue() {
-    if (this._isFilled()) return null;
-    return this.props.value;
-  }
-
-  _className() {
-    const cssClasses = ['square'];
-    if (this._isFilled())     cssClasses.push('is-filled');
-    if (this.props.isActive)  cssClasses.push('is-active');
-    return cssClasses.join(' ');
-  }
-
-  _isFilled() {
-    return this.props.value === filledSquareCharacter;
-  }
+export default function Square({
+  value,
+  isActive,
+  handleSquareClick,
+  dataTestid
+}) {
+  return (
+    <div
+      className={className(value, isActive)}
+      onClick={handleSquareClick}
+      data-testid={dataTestid}
+    >
+      {displayedValue(value)}
+    </div>
+  );
 }
 
-export default Square;
+function className(value, isActive) {
+  const cssClasses = ['square'];
+  if (isFilled(value)) cssClasses.push('is-filled');
+  if (isActive) cssClasses.push('is-active');
+  return cssClasses.join(' ');
+}
+
+function displayedValue(value) {
+  if (isFilled(value)) return null;
+  return value;
+}
+
+function isFilled(value) {
+  return value === filledSquareCharacter;
+}
