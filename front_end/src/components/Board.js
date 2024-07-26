b8fdab3796577cfc2af65ddd3a9ca1071314642e
Taylor Hummon <taylor.hummon@gmail.com>
2024-07-26 05:16:15 -0400

    modernizing Board and Options components

--- front_end/src/components/Board.js
+++ front_end/src/components/Board.js
@@ -1,65 +1,68 @@
-import React from 'react';
 import Square from './Square';
 import { boardWidth, boardHeight } from '../utilities/boardSize';
 import { indicesArray } from '../utilities/arrays';
 import './Board.css';
 
-class Board extends React.Component {
-  render() {
-    return (
-      <div
-        className={this._boardClassName()}
-        tabIndex="0"
-        onKeyDown={this.props.handleBoardKeyDown}
-        onFocus={this.props.handleBoardFocus}
-        onBlur={this.props.handleBoardBlur}
-        data-testid="board"
-      >
-        {this.renderRows()}
-      </div>
-    );
-  }
-
-  _boardClassName() {
-    const cssClasses = ['board'];
-    if (this.props.boardHasFocus) cssClasses.push('has-focus');
-    return cssClasses.join(' ');
-  }
-
-  renderRows() {
-    return indicesArray(boardHeight).map(
-      i => this.renderRow(i)
-    );
-  }
-
-  renderRow(i) {
-    return (
-      <div
-        className="board-row"
-        key={i}
-      >
-        {this.renderSquares(i)}
-      </div>
-    );
-  }
-
-  renderSquares(i) {
-    return indicesArray(boardWidth).map(
-      j => this.renderSquare(i * boardWidth + j)
-    );
-  }
-
-  renderSquare(k) {
-    return (
-      <Square
-        key={k}
-        value={this.props.squareValues[k]}
-        isActive={this.props.activeSquareIndex === k}
-        handleSquareClick={(event) => this.props.handleBoardClick(event, k)}
-        dataTestid={`square-${k}`}
-      />
-    );
-  }
+export default function Board({
+  squareValues,
+  activeSquareIndex,
+  boardHasFocus,
+  handleBoardKeyDown,
+  handleBoardClick,
+  handleBoardFocus,
+  handleBoardBlur
+}) {
+  return (
+    <div
+      className={className(boardHasFocus)}
+      tabIndex="0"
+      onKeyDown={handleBoardKeyDown}
+      onFocus={handleBoardFocus}
+      onBlur={handleBoardBlur}
+      data-testid="board"
+    >
+      {getRows(squareValues, activeSquareIndex, handleBoardClick)}
+    </div>
+  );
 }
 
-export default Board;
+function className(boardHasFocus) {
+  const cssClasses = ['board'];
+  if (boardHasFocus) cssClasses.push('has-focus');
+  return cssClasses.join(' ');
+}
+
+function getRows(squareValues, activeSquareIndex, handleBoardClick) {
+  return indicesArray(boardHeight).map(
+    i => getRow(squareValues, activeSquareIndex, handleBoardClick, i)
+  );
+}
+
+function getRow(squareValues, activeSquareIndex, handleBoardClick, i) {
+  return (
+    <div
+      className="board-row"
+      key={i}
+    >
+      {getSquares(squareValues, activeSquareIndex, handleBoardClick, i)}
+    </div>
+  );
+}
+
+function getSquares(squareValues, activeSquareIndex, handleBoardClick, i) {
+  return indicesArray(boardWidth).map(
+    j => getSquare(squareValues, activeSquareIndex, handleBoardClick, i * boardWidth + j)
+  );
+}
+
+function getSquare(squareValues, activeSquareIndex, handleBoardClick, k) {
+  return (
+    <Square
+      key={k}
+      value={squareValues[k]}
+      isActive={activeSquareIndex === k}
+      handleSquareClick={(event) => handleBoardClick(event, k)}
+      dataTestid={`square-${k}`}
+    />
+  );
+}
