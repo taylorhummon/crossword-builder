02fa41da75dd5fb1c162e575ff9b72e3787c32cc
Taylor Hummon <taylor.hummon@gmail.com>
2024-08-04 08:27:42 -0400

    factor out examples

--- fastapi/crossword_builder_api/models/suggestions_lists.py
+++ fastapi/crossword_builder_api/models/suggestions_lists.py
@@ -5,6 +5,14 @@ from pydantic import BaseModel, Field
 from crossword_builder_api.custom_typing import Letter, Tilde
 
 
+example_suggestions_list_in_params = {
+    "boardWidth": 2,
+    "boardHeight": 2,
+    "squareValues": ["A", "~", "B", None],
+    "activeSquareIndex": 0,
+    "canSuggestFill": True
+}
+
 class SuggestionsListInParams(BaseModel):
     boardWidth: int = Field(ge=1)
     boardHeight: int = Field(ge=1)
@@ -12,21 +20,11 @@ class SuggestionsListInParams(BaseModel):
     activeSquareIndex: int = Field(ge=0)
     canSuggestFill: bool
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "boardWidth": 2,
-                    "boardHeight": 2,
-                    "squareValues": ["A", "~", "B", None],
-                    "activeSquareIndex": 0,
-                    "canSuggestFill": True
-                }
-            ]
-        }
-    }
+    model_config = { "json_schema_extra": { "examples": [example_suggestions_list_in_params] } }
+
+example_suggestions_list_out_params = ["E", "Y", "~"]
 
 SuggestionsListOutParams = Annotated[
     list[Letter | Tilde],
-    Body(examples=[["E", "Y", "~"]])
+    Body(examples=[example_suggestions_list_out_params])
 ]
