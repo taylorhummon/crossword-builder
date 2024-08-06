a8545162da9b94066388fa57104e5283e6185fd9
Taylor Hummon <taylor.hummon@gmail.com>
2024-08-06 10:08:54 -0400

    remove typescript backend

--- back_end/src/services/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Application } from '../declarations';
-import { suggestions_lists } from './suggestions_lists/suggestions_lists';
-
-export const services = (app: Application) => {
-  app.configure(suggestions_lists);
-  // All services will be registered here
-}
--- back_end/src/services/suggestions_lists/suggestions_lists.shared.ts
+++
@@ -1,2 +0,0 @@
-export const suggestionsListsPath = '/suggestions-lists';
-export const suggestionslistsMethods = ['create'] as const;
