b8b9e5cf3e2c91ad759f590d3658110f9393616e
Taylor Hummon <taylor.hummon@gmail.com>
2024-08-06 10:04:47 -0400

    fastapi back end is hooked up and working

--- fastapi/crossword_builder_api/main.py
+++ fastapi/crossword_builder_api/main.py
@@ -1,13 +1,28 @@
 from __future__ import annotations
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from crossword_builder_api.models.suggestions_lists import SuggestionsListInParams, SuggestionsListOutParams
+from crossword_builder_api.lib.words import WordsFinder
+from crossword_builder_api.lib.suggestions import build_suggestions_list
 
 
 app = FastAPI()
+words_finder = WordsFinder()
+
+DEVELOPMENT_ORIGINS = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=DEVELOPMENT_ORIGINS,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post("/suggestions_lists", response_model=SuggestionsListOutParams)
 async def create_suggestions_list(
     params: SuggestionsListInParams
 ):
-    return frozenset(["A", "B", "Z", "~"])
+    return build_suggestions_list(words_finder, params)
--- front_end/src/lib/suggestions.ts
+++ front_end/src/lib/suggestions.ts
@@ -1,4 +1,3 @@
-import { getServer } from './server';
 import { State, RequestData } from '../declarations';
 import { BOARD_WIDTH, BOARD_HEIGHT } from '../lib/constants';
 import { arrayShallowEquivalent } from '../utilities/arrays';
@@ -36,18 +35,6 @@ export async function updateSuggestions(
   });
 }
 
-async function fetchSuggestions(
-  requestData: RequestData
-): Promise<Array<string>> {
-  try {
-    const server = getServer();
-    return await server.service('suggestions-lists').create(requestData);
-  } catch (error) {
-    console.log('Error occurred fetching suggestions', error);
-    return [];
-  };
-}
-
 function areSuggestionsOutdated(
   latestState: State,
   requestData: RequestData
@@ -56,4 +43,43 @@ function areSuggestionsOutdated(
   if (requestData.canSuggestFill !== latestState.canSuggestFill) return true;
   if (! arrayShallowEquivalent(requestData.squareValues, latestState.squareValues)) return true;
   return false;
+}
+
+async function fetchSuggestions(
+  requestData: RequestData
+): Promise<Array<string>> {
+  try {
+    const baseServerUri = process.env.REACT_APP_SERVER_URI;
+    if (baseServerUri === undefined) {
+      throw Error("REACT_APP_SERVER_URI environment variablem must be set");
+    }
+    const url = `${baseServerUri}/suggestions_lists`;
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(requestData)
+    };
+    const response = await fetch(url, options);
+    if (! response.ok) throw new Error(`Response status: ${response.status}`);
+    const jsonHash = await response.json();
+    return deserializeSuggestions(jsonHash);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+const SUGGESTION_REGULAR_EXPRESSION = /^([A-Z]|~)$/;
+
+function deserializeSuggestions(
+  jsonHash: any
+): Array<string> {
+  if (! Array.isArray(jsonHash)) throw new Error('Expected response to be an array', jsonHash);
+  const suggestions = [] as Array<string>;
+  for (const entry of jsonHash) {
+    if (typeof entry === "string" && SUGGESTION_REGULAR_EXPRESSION.test(entry)) {
+      suggestions.push(entry);
+    }
+  }
+  return suggestions;
 }
