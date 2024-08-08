f7596486a26ec74d8f880cb33384042cec5b76cc
Taylor Hummon <taylor.hummon@gmail.com>
2024-08-08 13:46:45 -0400

    a cleanup pass on suggestions functions

--- back_end/crossword_builder_api/lib/suggestions.py
+++ back_end/crossword_builder_api/lib/suggestions.py
@@ -17,71 +17,63 @@ def build_suggestions(
         squares=params.squares,
         active_index=params.activeIndex
     )
+    pattern_a = board.horizontal_pattern_through_active_square(
+        board.bound_left_of_active_square,
+        board.bound_right_of_active_square
+    )
+    pattern_b = board.vertical_pattern_through_active_square(
+        board.bound_above_active_square,
+        board.bound_below_active_square
+    )
     if params.canSuggestFill:
-        return _when_can_suggest_fill(words_finder, board)
+        return _suggestions_when_can_suggest_fill(words_finder, board, pattern_a, pattern_b)
     else:
-        return _when_cannot_suggest_fill(words_finder, board)
+        return _suggestions_when_cannot_suggest_fill(words_finder, pattern_a, pattern_b)
 
-def _when_cannot_suggest_fill(
+def _suggestions_when_cannot_suggest_fill(
     words_finder: WordsFinder,
-    board: Board
+    pattern_a: ActivePattern,
+    pattern_b: ActivePattern
 ) -> set[str]:
-    pattern_a = board.horizontal_pattern_through_active_square(
-        board.bound_left_of_active_square,
-        board.bound_right_of_active_square
-    )
-    pattern_b = board.vertical_pattern_through_active_square(
-        board.bound_above_active_square,
-        board.bound_below_active_square
-    )
-    suggestions_set_a = _get_suggestions_set_for_pattern(words_finder, pattern_a)
-    suggestions_set_b = _get_suggestions_set_for_pattern(words_finder, pattern_b)
-    return suggestions_set_a.intersection(suggestions_set_b)
+    suggestions_a = _suggestions_for_pattern(words_finder, pattern_a)
+    suggestions_b = _suggestions_for_pattern(words_finder, pattern_b)
+    return suggestions_a.intersection(suggestions_b)
 
-def _when_can_suggest_fill(
+def _suggestions_when_can_suggest_fill(
     words_finder: WordsFinder,
-    board: Board
+    board: Board,
+    pattern_a: ActivePattern,
+    pattern_b: ActivePattern
 ) -> set[str]:
-    pattern_a = board.horizontal_pattern_through_active_square(
-        board.bound_left_of_active_square,
-        board.bound_right_of_active_square
-    )
-    pattern_b = board.vertical_pattern_through_active_square(
-        board.bound_above_active_square,
-        board.bound_below_active_square
-    )
-    suggestions_set_a = _get_suggestions_set_for_all_subpatterns(words_finder, pattern_a)
-    suggestions_set_b = _get_suggestions_set_for_all_subpatterns(words_finder, pattern_b)
-    suggestions_set = suggestions_set_a.intersection(suggestions_set_b)
+    suggestions_a = _suggestions_for_all_subpatterns(words_finder, pattern_a)
+    suggestions_b = _suggestions_for_all_subpatterns(words_finder, pattern_b)
+    suggestions = suggestions_a.intersection(suggestions_b)
     if _will_suggest_fill(words_finder, board):
-        return suggestions_set.union([FILLED_SQUARE])
-    else:
-        return suggestions_set
+        suggestions.add(FILLED_SQUARE)
+    return suggestions
 
-def _get_suggestions_set_for_all_subpatterns(
+def _suggestions_for_all_subpatterns(
     words_finder: WordsFinder,
     pattern: ActivePattern
 ) -> set[str]:
-    suggestions_set = set()
-    subpatterns = pattern.subpatterns()
-    for subpattern in subpatterns:
-        for suggestion in _get_suggestions_set_for_pattern(words_finder, subpattern):
-            suggestions_set.add(suggestion)
-    return suggestions_set
+    return {
+        suggestion
+        for subpattern in pattern.subpatterns()
+        for suggestion in _suggestions_for_pattern(words_finder, subpattern)
+    }
 
-def _get_suggestions_set_for_pattern(
+def _suggestions_for_pattern(
     words_finder: WordsFinder,
     pattern: ActivePattern
 ) -> set[str]:
-    suggestions_set = set()
     i = pattern.active_index
     regular_expression = pattern.as_regular_expression()
     words = words_finder.words_of_length(len(pattern))
-    for word in words:
-        if regular_expression.match(word):
-            letter = word[i : i + 1]
-            suggestions_set.add(letter)
-    return suggestions_set
+    return {
+        word[i : i + 1]
+        for word in words
+        if regular_expression.match(word)
+    }
 
 def _will_suggest_fill(
     words_finder: WordsFinder,
@@ -91,29 +83,25 @@ def _will_suggest_fill(
         board.bound_left_of_active_square,
         board.active_column
     )
-    fill_ok_for_left = _will_suggest_fill_trim_left(words_finder, left_pattern)
-    if not fill_ok_for_left:
+    if not _will_suggest_fill_trim_left(words_finder, left_pattern):
         return False
     right_pattern = board.horizontal_pattern_through_active_square(
         board.active_column,
         board.bound_right_of_active_square
     )
-    fill_ok_for_right = _will_suggest_fill_trim_right(words_finder, right_pattern)
-    if not fill_ok_for_right:
+    if not _will_suggest_fill_trim_right(words_finder, right_pattern):
         return False
     top_pattern = board.vertical_pattern_through_active_square(
         board.bound_above_active_square,
         board.active_row
     )
-    fill_ok_for_top = _will_suggest_fill_trim_left(words_finder, top_pattern)
-    if not fill_ok_for_top:
+    if not _will_suggest_fill_trim_left(words_finder, top_pattern):
         return False
     bottom_pattern = board.vertical_pattern_through_active_square(
         board.active_row,
         board.bound_below_active_square
     )
-    fill_ok_for_bottom = _will_suggest_fill_trim_right(words_finder, bottom_pattern)
-    if not fill_ok_for_bottom:
+    if not _will_suggest_fill_trim_right(words_finder, bottom_pattern):
         return False
     return True
 
@@ -124,12 +112,15 @@ def _will_suggest_fill_trim_left(
     if pattern.active_index != len(pattern) - 1:
         raise Exception("expected last character to be active")
     subpatterns = pattern.subpatterns_trimming_left()
-    if any(len(subpattern) == 1 for subpattern in subpatterns):
+    if any(
+        len(subpattern) == 1
+        for subpattern in subpatterns
+    ):
         return True
-    for subpattern in subpatterns:
-        if _has_match(words_finder, subpattern.drop_last_character()):
-            return True
-    return False
+    return any(
+        _has_match(words_finder, subpattern.drop_last_character())
+        for subpattern in subpatterns
+    )
 
 def _will_suggest_fill_trim_right(
     words_finder: WordsFinder,
@@ -138,12 +129,15 @@ def _will_suggest_fill_trim_right(
     if pattern.active_index != 0:
         raise Exception("expected first character to be active")
     subpatterns = pattern.subpatterns_trimming_right()
-    if any(len(subpattern) == 1 for subpattern in subpatterns):
+    if any(
+        len(subpattern) == 1
+        for subpattern in subpatterns
+    ):
         return True
-    for subpattern in subpatterns:
-        if _has_match(words_finder, subpattern.drop_first_character()):
-            return True
-    return False
+    return any(
+        _has_match(words_finder, subpattern.drop_first_character())
+        for subpattern in subpatterns
+    )
 
 def _has_match(
     words_finder: WordsFinder,
@@ -151,7 +145,7 @@ def _has_match(
 ) -> bool:
     regular_expression = pattern.as_regular_expression()
     words = words_finder.words_of_length(len(pattern))
-    for word in words:
-        if regular_expression.match(word):
-            return True
-    return False
+    return any(
+        regular_expression.match(word)
+        for word in words
+    )
